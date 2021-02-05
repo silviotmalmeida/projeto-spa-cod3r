63de923858f1e02674469acf381401d74a3dff51
silviotmalmeida <65416796+silviotmalmeida@users.noreply.github.com>
2021-02-05 19:46:33 -0300

    Update startContainers.sh
    Ajustes gerais

--- startContainers.sh
+++ startContainers.sh
@@ -10,5 +10,5 @@ docker container exec spa bash -c "chmod 777 -R /usr/src/app"
 sleep 1
 
 echo "Iniciando o app..."
-docker container exec spa bash -c "cd /usr/src/app; npm i; npm audit fix; npm start"
+docker container exec spa bash -c "cd /usr/src/app; npm i; npm start"
 
